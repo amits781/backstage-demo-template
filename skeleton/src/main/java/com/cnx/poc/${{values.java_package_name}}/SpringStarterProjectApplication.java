f8a682de961f790e2eea54b88c3a62f0d6a58d19
Amit Kumar Sharma <amitkumar.sharma1@concentrix.com>
2024-06-20 15:01:23 +0530

    initial commit

---
+++ skeleton/src/main/java/com/cnx/poc/${{values.java_package_name}}/SpringStarterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cnx.poc.${{values.java_package_name}};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterProjectApplication.class, args);
+	}
+
+}
